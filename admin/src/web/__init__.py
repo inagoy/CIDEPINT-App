d48b5999d206b763dc9e06634356bf0ac18ee58c
Iñaki Goyeneche <igoyeneche.98@gmail.com>
2023-09-11 12:58:39 -0300

    add error handler

--- admin/src/web/__init__.py
+++ admin/src/web/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask import render_template
+from src.web import error
 
 def create_app(env="development", static_folder="../../static"):
     app = Flask(__name__, static_folder=static_folder)
@@ -7,5 +8,7 @@ def create_app(env="development", static_folder="../../static"):
     @app.get("/")
     def home():
         return render_template("home.html")
+
+    app.register_error_handler(404, error.not_found_error)
 
     return app
