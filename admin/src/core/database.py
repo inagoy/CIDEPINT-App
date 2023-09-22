6bc4e19a36b65ac2f67aabf91e709f107841b230
Iñaki Goyeneche <igoyeneche.98@gmail.com>
2023-09-22 20:30:24 -0300

    [conf] configuration for database and dotenv

--- admin/src/core/database.py
+++ admin/src/core/database.py
@@ -3,25 +3,26 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
 def init_app(app):
-        """
-        Inicialización de la aplicación.
-        """
-        db.init_app(app)
+    """
+    Inicialización de la aplicación.
+    """
+    db.init_app(app)
+    config_db(app)
 
 
 def config_db(app):
-        """
-        Configuración de la aplicación.
-        """
+    """
+    Configuración de la aplicación.
+    """
 
-        @app.teardown_request
-        def close_session(exception = None):
-            db.session.close()
+    @app.teardown_request
+    def close_session(exception = None):
+        db.session.close()
 
 
 def reset_db():
-        print("Eliminando base de datos...")
-        db.drop_all()
-        print("Creando base de datos...")
-        db.create_all()
-        print("Done!")
+    print("Eliminando base de datos...")
+    db.drop_all()
+    print("Creando base de datos...")
+    db.create_all()
+    print("Done!")
--- admin/src/web/__init__.py
+++ admin/src/web/__init__.py
@@ -2,16 +2,25 @@ from flask import Flask
 from flask import render_template
 from web.controllers import error_controller
 from web.routes import user_routes
-
+from src.core import database
+from src.web.config import config
 def create_app(env="development", static_folder="../../static"):
+    
     app = Flask(__name__, static_folder=static_folder)
-
-    @app.get("/")
-    def home():
-        return render_template("home.html")
+    app.config.from_object(config[env])
     
     app.register_blueprint(user_routes.user_bp)
 
     app.register_error_handler(404, error_controller.not_found_error)
+
+    database.init_app(app)
+
+    @app.cli.command(name="resetdb")
+    def resetdb():
+        database.reset_db()
+
+    @app.get("/")
+    def home():
+        return render_template("home.html")
 
     return app
