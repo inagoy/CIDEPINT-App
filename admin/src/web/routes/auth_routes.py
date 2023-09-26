e3562ec7149792f3093c4b0812c17b2daa9f81f6
Julia Saenz <julisaenz99@gmail.com>
2023-09-26 18:44:30 -0300

    [fix] PEP8 compliant

--- admin/src/web/routes/auth_routes.py
+++ admin/src/web/routes/auth_routes.py
@@ -1,9 +1,11 @@
 from flask import Blueprint, render_template
 auth_bp = Blueprint('auth', __name__)
 
+
 @auth_bp.route('/registro', methods=['GET', 'POST'])
 def register():
     return render_template('registro.html')
+
 
 @auth_bp.route('/iniciar-sesion', methods=['GET', 'POST'])
 def login():
