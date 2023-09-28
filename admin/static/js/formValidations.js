7b6248317bfa19af350e6ecffbc4982d58cbea61
Julia Saenz <julisaenz99@gmail.com>
2023-09-28 14:55:06 -0300

    [fix] login validations

---
+++ admin/static/js/formValidations.js
@@ -0,0 +1,41 @@
+/**
+ * Validates an email address entered in an input field and displays an error message if the email format is invalid.
+ *
+ * @param {string} inputId - The id of the input field containing the email address.
+ * @param {string} errorId - The id of the element where the error message should be displayed.
+ * @return {void} This function does not return anything.
+ */
+function validateEmail(inputId, errorId) {
+	const emailInput = document.getElementById(inputId);
+	const emailError = document.getElementById(errorId);
+	const emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+
+	if (!emailPattern.test(emailInput.value)) {
+		emailError.textContent = "Formato de mail inválido. Intente nuevamente";
+	} else {
+		emailError.textContent = "";
+	}
+}
+
+function validatePassword(inputId, errorId) {
+	const passwordInput = document.getElementById(inputId);
+	const passwordError = document.getElementById(errorId);
+	const password = passwordInput.value;
+
+	if (password.length < 6) {
+		passwordError.textContent = "La contraseña debe tener por lo menos 6 caracteres.";
+		return;
+	}
+
+	if (!/\d/.test(password)) {
+		passwordError.textContent = "La contraseña debe contener por lo menos un número";
+		return;
+	}
+
+	if (!/[A-Z]/.test(password)) {
+		passwordError.textContent = "La contraseña debe contener por lo menos una mayúscula";
+		return;
+	}
+
+	passwordError.textContent = "";
+}
