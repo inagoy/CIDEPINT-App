cca3cb9bd67dc2f23edf95876c9fe52b72053823
Julia Saenz <julisaenz99@gmail.com>
2023-11-14 14:26:22 -0300

    map integration

--- portal/src/components/cards/MapCard.vue
+++ portal/src/components/cards/MapCard.vue
@@ -1,16 +1,37 @@
 <script>
+import { onMounted } from 'vue'
+
 export default {
   name: 'Map',
   props: {
     location: {
       type: String
     }
+  },
+  setup() {
+    let map, marker
+    onMounted(() => {
+      map = L.map('map').setView([-34.9223, -57.9546], 12)
+      L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        maxZoom: 19,
+        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+      }).addTo(map)
+      marker = L.marker([-34.9223, -57.9546]).addTo(map);
+      marker.bindPopup("<b>Dirección:</b> aaaaa <br/> <b>Contacto:</b> aaaaa").openPopup();
+    })
   }
 }
 </script>
 
 <template>
-  <div class="bg-info my-4">
-    <h6 class="fw-bold"> Mapa </h6>
+  <div class=" my-4">
+    <h6 class="fw-bold"> Cómo llegar </h6>
+    <div id="map"></div>
   </div>
 </template>
+
+<style scoped>
+#map {
+  height: 220px;
+}
+</style>
