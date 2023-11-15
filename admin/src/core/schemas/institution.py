54b4c9813255139ee279407fd2438c4a467b5bca
Julia Saenz <julisaenz99@gmail.com>
2023-11-15 13:55:21 -0300

    api get institution by name

--- admin/src/core/schemas/institution.py
+++ admin/src/core/schemas/institution.py
@@ -8,6 +8,7 @@ class InstitutionModelSchema(BaseSchema):
     address = fields.Str()
     location = fields.Str()
     web = fields.Str(attribute='website')
+    coordinates = fields.Str(attribute='coordinates')
     days_and_opening_hours = fields.Str(attribute='days_and_hours')
     email = fields.Email(attribute='contact_info')
     enabled = fields.Boolean()
