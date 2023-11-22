9d6597a1b76a6cabbcab3da4404e510a54327626
Iñaki Goyeneche <igoyeneche.98@gmail.com>
2023-11-22 20:20:07 -0300

    [feat] adding google login from public app

--- admin/src/core/api/auth.py
+++ admin/src/core/api/auth.py
@@ -1,9 +1,12 @@
 from flask import Blueprint, request
 from marshmallow import ValidationError
 from src.core.schemas.user import UserValidateSchema
-from src.web.helpers.auth import check_user
+from src.web.helpers.auth import check_user, check_google_user
 from flask_jwt_extended import create_access_token
 from flask_cors import cross_origin
+from google.oauth2 import id_token
+from google.auth.transport import requests
+
 
 api_auth_bp = Blueprint('api_auth', __name__, url_prefix='/api')
 
@@ -24,5 +27,21 @@ def api_auth():
     )
     if not user:
         return {"error": "El usuario o la contraseña son incorrectos"}, 401
+    access_token = create_access_token(identity=user.id)
+    return {"token": access_token}, 201
+
+
+@api_auth_bp.route("/auth-google", methods=["POST"])
+@cross_origin()
+def api_auth_google():
+    authinfo = request.get_json()
+    idinfo = id_token.verify_oauth2_token(authinfo['credential'],
+                                          requests.Request(),
+                                          authinfo['client_id'])
+    if not idinfo:
+        return {"error": "Unauthorized"}, 401
+    user = check_google_user(idinfo['email'])
+    if not user:
+        return {"error": "El usuario no está registrado"}, 401
     access_token = create_access_token(identity=user.id)
     return {"token": access_token}, 201
